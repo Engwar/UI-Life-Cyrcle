97c571500437a00fa114eed748d786aac2b8f9bd
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-12 15:16:30 +0300

    Initial Commit

---
+++ UI Life Cyrcle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI Life Cyrcle
+//
+//  Created by Igor Shelginskiy on 12/10/2018.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
